2ac96d4a7c5a692b46a2709e8ec066a7ac80ff34
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-10-08 07:30:34 +0200

    Constructors added for ShadingSolverOptions

--- DiGi.Solar.ComputeSharp/Classes/ShadingSolverOptions.cs
+++ DiGi.Solar.ComputeSharp/Classes/ShadingSolverOptions.cs
@@ -2,6 +2,7 @@
 using DiGi.Core.Interfaces;
 using DiGi.Solar.Enums;
 using DiGi.Solar.Interfaces;
+using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 
 namespace DiGi.Solar.ComputeSharp.Classes
@@ -19,5 +20,29 @@ namespace DiGi.Solar.ComputeSharp.Classes
 
         [JsonInclude, JsonPropertyName("Tolerance")]
         public double Tolerance { get; set; } = Core.Constans.Tolerance.Distance;
+
+        public ShadingSolverOptions(ShadingSolverOptions? shadingSolverOptions)
+            :base(shadingSolverOptions)
+        {
+            if(shadingSolverOptions != null)
+            {
+                AngleTolerance = shadingSolverOptions.AngleTolerance;
+                ShadingSolverType = shadingSolverOptions.ShadingSolverType;
+                TimeSeries = Core.Query.Clone(shadingSolverOptions.TimeSeries) ?? new DateTimeSeries(DateTime.Now);
+                Tolerance = shadingSolverOptions.Tolerance;
+            }
+        }
+
+        public ShadingSolverOptions(JsonObject? jsonObject)
+            : base(jsonObject)
+        {
+
+        }
+
+        public ShadingSolverOptions() 
+            : base() 
+        { 
+
+        }
     }
 }
